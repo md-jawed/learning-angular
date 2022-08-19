ce762d452909ffcb2ebaaa7e01a9c4d30ea8cca1
md.jawed <md.jawed@cropin.in>
2022-08-19 12:36:42 +0530

    First Helloworld commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'learning-angular';
+// }
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'hello-world',
+  template: `
+    <h2>Hello World</h2>
+    <p>This is my first component!</p>
+  `
 })
-export class AppComponent {
-  title = 'learning-angular';
+export class HelloWorldComponent {
+  // The code in this class drives the component's behavior.
 }
