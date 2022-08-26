eabc387137162bd750b9d7fd72f85d3d648d019a
Aqueeb <md.jawed@cropin.com>
2022-08-26 19:47:27 +0530

    Side-nav & Nav-bar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,12 +5,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from  '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 const MaterialComponents = [
   MatButtonModule,
   MatFormFieldModule,
   MatInputModule,
-  MatSelectModule
+  MatSelectModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule
 ];
 @NgModule({
   imports: [ MaterialComponents  ],
