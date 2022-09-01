f55af89938857e988b1f26f8b87af38bbaa57681
Aqueeb <md.jawed@cropin.com>
2022-09-01 11:22:26 +0530

    CAD for Number format

--- src/app/registration-form/number-format.directive.ts
+++ src/app/registration-form/number-format.directive.ts
@@ -1,20 +1,25 @@
-import { Directive,ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Directive,ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 
 @Directive({
   selector: '[appNumberFormat]'
 })
 export class NumberFormatDirective implements OnChanges{
-  @Input() val!:string;
+  @Input() myval:any;
+  
+  constructor(public el: ElementRef) { 
+    // el.nativeElement.style.color = 'red';
+    // el.nativeElement.value = this.myval;
+    // console.log(this.myval);
+    // 10 -> 10.00 or 10.678 -> 10.68 
+  }
   ngOnChanges() {
     // this.el.nativeElement.value = 'Test';
-    
-    console.log(this.val);
-  }
-  constructor(public el: ElementRef) { 
-    el.nativeElement.style.color = 'red';
-    el.nativeElement.value = this.val;
-    console.log(this.val);
-    // 10 -> 10.00 or 10.678 -> 10.68 
+    // console.log(this.el.nativeElement.myval);
+    // this.el.nativeElement.value = 'Test';
+    this.el.nativeElement.style.color = 'red';
+    this.el.nativeElement.myval = parseFloat(this.myval).toFixed(2);
+    console.log(this.el.nativeElement.myval);
+    this.el.nativeElement.innerHTML = this.el.nativeElement.myval;
   }
 
 }
