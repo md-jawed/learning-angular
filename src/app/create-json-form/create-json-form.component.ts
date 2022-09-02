3d4b9ab8b8d7ebf9dc29eb0090f0c7ecdff3568f
Aqueeb <md.jawed@cropin.com>
2022-09-02 18:21:58 +0530

    Custom Dynamic form using JSON Completed

--- src/app/create-json-form/create-json-form.component.ts
+++ src/app/create-json-form/create-json-form.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export interface formControl {
   name: string;
@@ -8,6 +8,12 @@ export interface formControl {
   value: string;
   type: string;
   options?: string;
+  validators: JsonValidators;
+}
+export interface JsonValidators {
+  min?: number;
+  max?: number;
+  required?: boolean;
 }
 export interface JsonFormField {
   controls: formControl[];
@@ -17,34 +23,52 @@ export interface JsonFormField {
   selector: 'app-create-json-form',
   templateUrl: './create-json-form.component.html',
   styleUrls: ['./create-json-form.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CreateJsonFormComponent implements OnChanges {
-
+export class CreateJsonFormComponent implements OnChanges,OnInit {
+  public submitted = false;
   @Input() formFields!: JsonFormField;
   public jsonForm: FormGroup = this.fb.group({});
   constructor( private fb: FormBuilder) {}
 
+  ngOnInit(){
+    this.createForm(this.formFields.controls);
+  }
   
   ngOnChanges(changes: SimpleChanges): void {
-    if (!changes['formFields'].firstChange) {
-      this.createForm(this.formFields.controls);
-      console.log("Change:",this.formFields);
-    }
+    // if (changes['formFields'].firstChange) {
+    //   this.createForm(this.formFields.controls);
+    //   // console.log("Change:",this.formFields.controls);
+    // }
     // console.log(this.formFields);
   }
   createForm(controls: formControl[]){
     for (const control of controls){
+      const jsonvalid = []
+      for(const [key, value] of Object.entries(control.validators)){
+        switch(key){
+          case 'min':
+            jsonvalid.push(Validators.min(value));
+            break;
+          case 'max':
+            jsonvalid.push(Validators.max(value));
+            break;
+          case 'required':
+            jsonvalid.push(Validators.required);
+            break;
+        }
+      }
       this.jsonForm.addControl(
         control.name,
-        this.fb.control('')
+        this.fb.control(control.value,jsonvalid)
       );
-      console.log("Create form called.")
+      // console.log("Create Form");
     }
-    console.log(this.formFields);
+    // console.log(this.formFields);
   }
   onSubmit(){
     console.log("Form Submitted");
+    this.submitted = true;
   }
 
 }
--- src/app/jsonform/jsonform.component.ts
+++ src/app/jsonform/jsonform.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { JsonFormField } from '../create-json-form/create-json-form.component';
-
+import * as jsonForm from '../../assets/form-fields.json' ; 
 @Component({
   selector: 'app-jsonform',
   templateUrl: './jsonform.component.html',
@@ -9,16 +9,26 @@ import { JsonFormField } from '../create-json-form/create-json-form.component';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class JSONFormComponent implements OnInit {
-
+  public available: boolean = false;
   public formData!: JsonFormField;
-  constructor(private http: HttpClient) {}
-  ngOnInit() {
-    this.http
-      .get('../../assets/form-fields.json')
+  constructor(private http: HttpClient) {
+    
 
-      .subscribe((form: any) => {
-        this.formData = form;
-        // console.log(this.formData);
-      });
   }
+  ngOnInit() {
+    // this.http
+    //   .get('../../assets/form-fields.json')
+
+    //   .subscribe((form: any) => {
+    //     this.formData = form;
+    //     console.log(this.formData);
+    //     this.available=true;
+    //     console.log(this.available);
+    //   });
+    // console.log(jsonForm);
+    this.formData = jsonForm;
+    this.available = true;
+    // console.log(this.formData);
+  }
+  
 }
